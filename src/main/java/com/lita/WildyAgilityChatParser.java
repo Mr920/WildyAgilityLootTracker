edab607d7a7be3b07d7cb4958bb2410c5cf999f3
Charles Lentz <charles_lentz@hotmail.com>
2026-08-02 02:50:51 -0500

    Simplified WildyAgilityChatParser as much as I could for the moment, though it seems like it could still use further simplification. But this should be enough for now.

--- src/main/java/com/lita/WildyAgilityChatParser.java
+++ src/main/java/com/lita/WildyAgilityChatParser.java
@@ -1,103 +1,69 @@
 package com.lita;
-
-import lombok.extern.slf4j.Slf4j;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-@Slf4j
 public class WildyAgilityChatParser {
-
     public WildyAgilityLootTrackerPlugin plugin;
-
-    public Pattern awardP;
-    public Pattern highlightP;
-    public Pattern itemP;
-    public Pattern rewardStreakP;
-    public Pattern lapCountP;
-
-    public String  parseMessage;
-    public Matcher currentMatch;
-
+    public                       Pattern awardP;
+    public                       Pattern highlightP;
+    public                       Pattern itemP;
+    public                       Pattern rewardStreakP;
+    public                       Pattern lapCountP;
+    public                        String parseMessage;
+    public                       Matcher currentMatch;
     public WildyAgilityChatParser(WildyAgilityLootTrackerPlugin _plugin){
         plugin        = _plugin;
-        log.debug("Instantiating");
         awardP        = Pattern.compile("You have been awarded");
         highlightP    = Pattern.compile("[<]col[=]ef1020[>]([^<]+)[<].col[>]");
         itemP         = Pattern.compile("([0-9]+) x (.+)");
         rewardStreakP = Pattern.compile("Wilderness Agility reward streak is: .col=ff0000.([0-9]+)");
         lapCountP     = Pattern.compile("Wilderness Agility lap count is: .col=ff0000.([0-9]+)");
     }
-
     public int[] badIDs(){ return new int[] {1, 0}; }
-
+    private boolean checkMessageMatches(String chatMessage, Pattern p){
+        parseMessage = chatMessage;
+        currentMatch = p.matcher(parseMessage);
+        return currentMatch.find();
+    }
     public boolean checkIsAwardMessage(String chatMessage){
-        this.parseMessage = chatMessage;
-        this.currentMatch = awardP.matcher(chatMessage);
-        if (! this.currentMatch.find()){
-            //log.debug("Game Message does not match the award text pattern");
-            //log.debug(String.format("Game Message was: '%s'", msg));
-            return false;
-        }
-        log.debug("Received Game Message matching the award format pattern, proceeding to parse it out.");
-        LtaLootItem[] mutatedObjects = parseAwardMessage(chatMessage);
-        this.plugin.onDataMutation(mutatedObjects);
-        return true;
+        if (checkMessageMatches(chatMessage, awardP)) {        plugin.onDataMutation(parseAwardMessage());   return true; } else { return false; }
     }
     public boolean checkIsLapCountMessage(String chatMessage){
-        this.parseMessage = chatMessage;
-        this.currentMatch = lapCountP.matcher(chatMessage);
-        if (! this.currentMatch.find()){ return false; }
-        this.plugin.updateLapCount(this.currentMatch.group(1));
-        return true;
+        if (checkMessageMatches(chatMessage, lapCountP)) {     plugin.updateLapCount(currentMatch.group(1)); return true; } else { return false; }
     }
     public boolean checkIsStreakMessage(String chatMessage){
-        this.parseMessage = chatMessage;
-        this.currentMatch = rewardStreakP.matcher(chatMessage);
-        if (! this.currentMatch.find()){ return false; }
-        this.plugin.updateStreak(this.currentMatch.group(1));
-        return true;
+        if (checkMessageMatches(chatMessage, rewardStreakP)) { plugin.updateStreak(currentMatch.group(1));   return true; } else { return false; }
     }
-
-    public LtaLootItem[] parseAwardMessage(String msg){
-        this.parseMessage = msg;
-        this.currentMatch = highlightP.matcher(msg);
-        LtaLootItem updatedSupplyItem = parseOut_SupplyItem(this.currentMatch);
-        LtaLootItem updatedArmourItem = parseOut_ArmourItem(this.currentMatch);
-        //log.debug(String.format("Parsed -> (Supply : Qty=%s , Name=%s) : (Armour : Qty=%s, Name=%s)", supplyQtyS, supplyItemS, armourQtyS, armourItemS));
-        log.info(String.format("Parsed & Updated [S] -> %s", updatedSupplyItem.toDebugString()));
-        log.info(String.format("Parsed & Updated [A] -> %s", updatedArmourItem.toDebugString()));
-        this.plugin.printCheckPointBanner();
-        return new LtaLootItem[] {
-                updatedSupplyItem,
-                updatedArmourItem
-        };
+    public LtaLootItem[] parseAwardMessage(){
+        LtaLootItem updatedSupplyItem = null;
+        LtaLootItem updatedArmourItem = null;
+        currentMatch = highlightP.matcher(parseMessage);
+        if (currentMatch.find()) { updatedSupplyItem = parseOut_SupplyItem(); }
+        if (currentMatch.find()) { updatedArmourItem = parseOut_ArmourItem(); }
+        plugin.printCheckPointBanner();
+        return new LtaLootItem[]{updatedSupplyItem, updatedArmourItem};
     }
-    public int[]       parseOut_LootItem(Matcher highlightMatcher){
-        if (! highlightMatcher.find()) { log.debug("Failed to match highlight pattern"); return badIDs(); } // i know this will bite me in the ass, I'll fix it later
-        String         hStr = highlightMatcher.group(1);
-        Matcher itemMatcher = itemP.matcher(hStr);
-        if (! itemMatcher.find()) { log.debug("Failed to match item pattern"); return badIDs(); }           // i know this will bite me in the ass, I'll fix it later
-        String         QtyS = itemMatcher.group(1);
-        String        ItemS = itemMatcher.group(2);
-        int          ItemId = LtaLootItem.getItemIdFromName(ItemS);
-        int             Qty = Integer.parseInt(QtyS);
+    public int[]       parseOut_LootItem(){
+        Matcher itemMatcher = itemP.matcher(currentMatch.group(1));
+        if (! itemMatcher.find()) { return badIDs(); } // i know this will bite me in the ass, I'll fix it later
+        int             Qty = Integer.parseInt(itemMatcher.group(1));
+        int          ItemId = LtaLootItem.getItemIdFromName(itemMatcher.group(2));
         return new int[] { ItemId, Qty };
     }
-    public LtaLootItem parseOut_SupplyItem(Matcher highlightMatcher){
-        int[]       parsedValues = parseOut_LootItem(highlightMatcher);
+    public LtaLootItem parseOut_SupplyItem(){
+        int[]       parsedValues = parseOut_LootItem();
         int               itemId = parsedValues[0];
         int                  Qty = parsedValues[1];
-        LtaLootItem supplyItem   = this.plugin.getMatchingSupplyItem(itemId);
+        LtaLootItem   supplyItem = plugin.getMatchingSupplyItem(itemId);
         supplyItem.haveQuantity += Qty;
         return supplyItem;
     }
-    public LtaLootItem parseOut_ArmourItem(Matcher highlightMatcher){
-        int[]       parsedValues = parseOut_LootItem(highlightMatcher);
+    public LtaLootItem parseOut_ArmourItem(){
+        int[]       parsedValues = parseOut_LootItem();
         int               itemId = parsedValues[0];
         int                  Qty = parsedValues[1];
-        LtaLootItem   armourItem = this.plugin.getMatchingArmourItem(itemId);
+        LtaLootItem   armourItem = plugin.getMatchingArmourItem(itemId);
         armourItem.haveQuantity += Qty;
         return armourItem;
     }
-
 }
