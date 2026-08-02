e060068a3ea09616a88cb778042d9318fa426018
Charles Lentz <charles_lentz@hotmail.com>
2026-08-02 07:26:26 -0500

    Simplified WildyAgilityLootTrackerConfig

--- src/main/java/com/lita/WildyAgilityLootTrackerConfig.java
+++ src/main/java/com/lita/WildyAgilityLootTrackerConfig.java
@@ -1,49 +1,24 @@
 package com.lita;
-
 import net.runelite.client.config.Config;
 import net.runelite.client.config.ConfigGroup;
 import net.runelite.client.config.ConfigItem;
 
 @ConfigGroup("WildyAgilityLootTracker")
-public interface WildyAgilityLootTrackerConfig extends Config
-{
+public interface WildyAgilityLootTrackerConfig extends Config {
     public boolean _showSupplies      = true;
     public boolean _showSteelArmour   = true;
     public boolean _showMithrilArmour = true;
     public boolean _showAdamantArmour = true;
     public boolean _showRuneArmour    = true;
 
-    /*
-    @ConfigItem(
-        keyName = "greeting",
-        name = "Welcome Greeting",
-        description = "The message to show to the user when they login"
-    )
-    default String greeting()
-    {
-        return "Hello";
-    }
-    */
-
     @ConfigItem(keyName = "ShowSupplies",      name="Show Supplies",       description = "Whether or not to track and display supply items")
-    default boolean getShowSupplies(){
-        return _showSupplies;
-    }
+    default boolean getShowSupplies(){       return _showSupplies;        }
     @ConfigItem(keyName = "ShowSteelArmour",   name="Show Steel Armour",   description = "Whether or not to track and display steel armour items")
-    default boolean getShowSteelArmour(){
-        return _showSteelArmour;
-    }
+    default boolean getShowSteelArmour(){    return _showSteelArmour;     }
     @ConfigItem(keyName = "ShowMithrilArmour", name="Show Mithril Armour", description = "Whether or not to track and display mithril armour items")
-    default boolean getShowMithrilArmour(){
-        return _showMithrilArmour;
-    }
+    default boolean getShowMithrilArmour(){  return _showMithrilArmour;   }
     @ConfigItem(keyName = "ShowAdamantArmour", name="Show Adamant Armour", description = "Whether or not to track and display adamant armour items")
-    default boolean getShowAdamantArmour(){
-        return _showAdamantArmour;
-    }
+    default boolean getShowAdamantArmour(){  return _showAdamantArmour;   }
     @ConfigItem(keyName = "ShowRuneArmour",    name="Show Rune Armour",    description = "Whether or not to track and display rune armour items")
-    default boolean getShowRuneArmour(){
-        return _showRuneArmour;
-    }
-
+    default boolean getShowRuneArmour(){     return _showRuneArmour;      }
 }
