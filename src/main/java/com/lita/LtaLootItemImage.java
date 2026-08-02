ab8fa3363c576502eed88c13f80e2d22db6b70b0
Charles Lentz <charles_lentz@hotmail.com>
2026-08-02 03:18:39 -0500

    Simplified LtaLootItemImage

--- src/main/java/com/lita/LtaLootItemImage.java
+++ src/main/java/com/lita/LtaLootItemImage.java
@@ -1,14 +1,10 @@
 package com.lita;
-
 import net.runelite.client.ui.overlay.components.ImageComponent;
-import net.runelite.client.util.AsyncBufferedImage;
 
 public class LtaLootItemImage extends ImageComponent {
-
-    public LtaLootItem        targetLootItem  = null;
-
+    public LtaLootItem targetLootItem = null;
     public LtaLootItemImage(LtaLootItem lootItem){
         super(lootItem.getQtyImage());
-        this.targetLootItem  = lootItem;
+        this.targetLootItem = lootItem;
     }
 }
